f5b2ce3046b667900e3de59be8f51c04b26fa397
akimany <akimany@gmail.com>
2021-07-01 13:17:23 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "akimany-gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-2SMHBK6FTJ",
+      },
+    },
+  ],
+};
